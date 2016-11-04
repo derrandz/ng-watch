f30f99bf62eca0c5de2a187c80985560589df27b
Hamza Ouaghad <ouaghad.hamza@gmail.com>
2016-11-04 19:58:05 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# ng-server-by
+A better way than controllers
+
+## What is ngWatch ?
+
+**ngWatch** is a directive that facilitates tying code parts together using watch.
+
+## Usage?
+```Jade
+	div(watch="myVar", on-change="reloadCollection()", deep="false/true")
+```
+### Example of a real world use case:
+```Jade
+Incoming...
+```
---
+++ directives/ngWatch.js
@@ -0,0 +1,43 @@
+(function (angular) {
+    'use strict';
+
+    var app = angular.module('ngWatch');
+
+    /**
+     * Eases up the use watching elements and executing certain code upon firing change.
+     * This proves particularly useful when tying multiple parts together
+     * @param $parse
+     * @example
+        div(ngWatch="myCollection", on-change="displayPopup()", deep="true")
+     */
+    var ngWatch = function ($parse) {
+        return {
+            restrict: 'A',
+            scope: true,
+            compile: function (scope, element, attrs) {
+                return {
+                    pre: function (scope, element, attrs) {
+                        if (!attrs.ngWatch) {
+                            throw 'ngWatch directive needs to receive a valid variable, an empty string received';
+                        }
+
+                        if (!attrs.onChange) {
+                            throw 'ngWatch directive requires the use of on-change attribute!';
+                        }
+
+                        scope.$ngWatch(function () {
+                            return $parse(attrs.ngWatch, null, true)(scope);
+                        }, function () {
+                            $parse(attrs.onChange, null, true)(scope);
+                        }, attrs.collection === 'true');
+                    }
+                };
+            }
+        };
+    };
+
+    /**
+     * ngWatch directive Registered
+     */
+    app.directive('ngWatch', ngWatch);
+}(angular));
